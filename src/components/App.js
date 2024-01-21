f162c24dcff03f03260f533b12186c86fb49f31c
deepanshu000001 <143742351+deepanshu000001@users.noreply.github.com>
2024-01-21 17:26:37 +0530

    Update

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
-import React, { useReducer } from 'react'
-import { counterReducer } from '../reducers/counterReducer';
+import React, { useReducer } from 'react';
+import counterReducer from '../reducers/counterReducer';
 import '../styles/App.css';
+
 const App = () => {
-const [state,dispatch] = useReducer(counterReducer,{/*initial state to be placed here*/})
+  // Initial state for the counter
+  const initialState = { count: 0 };
+
+  // useReducer hook with the counterReducer and initial state
+  const [state, dispatch] = useReducer(counterReducer, initialState);
+
   return (
     <div id="main">
-
+      <span id="counter">{state.count}</span>
+      <button id="increment-btn" onClick={() => dispatch({ type: 'INCREMENT' })}>
+        Increment
+      </button>
+      <button id="decrement-btn" onClick={() => dispatch({ type: 'DECREMENT' })}>
+        Decrement
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
